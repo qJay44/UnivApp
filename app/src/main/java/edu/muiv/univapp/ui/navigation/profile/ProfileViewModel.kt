90a2bfec7d44be757eb15b71453279b1a3267ec5
qJay44 <m70155302@yandex.ru>
2022-12-18 13:47:14 +0300

    profile fragment front-end rework

--- app/src/main/java/edu/muiv/univapp/ui/navigation/profile/ProfileViewModel.kt
+++ app/src/main/java/edu/muiv/univapp/ui/navigation/profile/ProfileViewModel.kt
@@ -26,7 +26,7 @@ class ProfileViewModel : ViewModel() {
     private var visitAmount = 0
 
     val attendanceAmount: String
-        get() = " $visitAmount / $scheduleAllSize"
+        get() = " $visitAmount/$scheduleAllSize пар"
 
     val attendancePercent: String
         get() {
